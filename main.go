b83eef7b13ea504703512660aa4ef22c5135f386
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2025-04-15 13:54:35 +0100

    ls & the flags l, a

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"os/user"
+	"strconv"
+)
+
+func main(){
+	// args := os.Args
+	dir := "."
+	// ls(dir)
+	a(dir)
+	// l(dir)
+}
+
+func ls(FileName string){
+	colors := map[string]string{
+		"blue":    "\033[94m",
+		"reset":   "\033[0m",
+	}
+	fileInfo, err := os.Stat(FileName)
+	if err != nil{
+		fmt.Println("error in the os.Stat function :", err)
+	}
+	if fileInfo.IsDir(){
+		content, err1 := os.ReadDir(FileName)
+		if err1 != nil{
+			fmt.Println("error opening the folder")
+			return
+		}
+		for _, x := range content{
+				if x.Name()[0] == '.'{
+					continue
+				}else if x.IsDir(){
+					temp := colors["blue"] + x.Name() + colors["reset"]
+					fmt.Println(temp)
+				}else{
+					fmt.Println(x.Name())
+				}
+		}
+	}else{
+		fmt.Println(fileInfo.Name())
+	}
+}
+
+func a(FileName string){
+	colors := map[string]string{
+		"blue":    "\033[94m",
+		"reset":   "\033[0m",
+	}
+	fileInfo, err := os.Stat(FileName)
+	if err != nil{
+		fmt.Println("error in the os.Stat function :", err)
+	}
+	if fileInfo.IsDir(){
+		fmt.Println(colors["blue"] + "." + colors["reset"])
+		fmt.Println(colors["blue"] + ".." + colors["reset"])
+		content, err1 := os.ReadDir(FileName)
+		if err1 != nil{
+			fmt.Println("error opening the folder")
+			return
+		}
+		for _, x := range content{
+				if x.IsDir(){
+					temp := colors["blue"] + x.Name() + colors["reset"]
+					fmt.Println(temp)
+				}else{
+					fmt.Println(x.Name())
+				}
+		}
+	}else{
+		fmt.Println(fileInfo.Name())
+	}
+	
+}
+
+func l(FileName string){
+	FileInfo, err := os.Stat(FileName)
+	if err != nil{
+		fmt.Println("Error in the 'l' function, Os.stat")
+	}
+	test, err1 := FileInfo.Sys().(*syscall.Stat_t) //err1 here is a bool
+	if !err1{
+		fmt.Println("syscall struct error, go check it")
+	}
+
+	fmt.Print(FileInfo.Mode(), " ")
+	fmt.Print(test.Nlink, " ")
+	userName, err2 := user.LookupId(strconv.Itoa(int(test.Uid)))
+	if err2 != nil {
+		fmt.Println("Error in LookupId, go check it")
+	}
+	fmt.Print(userName.Username, " ")
+	groupName, err2 := user.LookupGroupId(strconv.Itoa(int(test.Gid)))
+	if err2 != nil {
+		fmt.Println("Error in LookupId, go check it")
+	}
+	fmt.Print(groupName.Name, " ")
+	fmt.Print(test.Size, " ")
+	time := FileInfo.ModTime()
+	fmt.Print(time.Format("Jan 02 15:04"), " ") // January 2nd, 2006 at 3:04:05 PM (MST) — is Go’s reference time.
+	fmt.Println(FileInfo.Name())
+
+}
