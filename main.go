cc494b14dc36af11212cabc59292f841e930fb84
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2025-04-21 15:59:00 +0100

    modify the mian

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	functions "myls1/functions"
+	"os"
 )
 
 func main() {
-	// args := os.Args
-	// Themap, files := argument_check(args)
-	// fmt.Println(args)
+	args := os.Args
+	Themap, _ := functions.ArgumentChecking(args)
 	// fmt.Println(Themap)
 	// fmt.Println(files)
-	dir := "."
-	// ls(dir)
-	// a(dir)
-	// l(dir)
-	// Bfs(dir)
-	// r(dir)
-    
-	// result := functions.LS(dir)
-    // fmt.Println(result)
-	// test the t flag
-	result := functions.T(dir)
 
-	fmt.Println(functions.Filter(result))
+	if Themap["recursive"]{
+		Themap["R"] = true
+	}
+	if Themap["time"]{
+		Themap["t"] = true
+	}
+	if Themap["all"]{
+		Themap["ls"] = true
+		Themap["l"] = true
+		Themap["R"] = true
+		Themap["r"] = true
+		Themap["a"] = true
+		Themap["t"] = true
+	}
+	if Themap["reverse"]{
+		Themap["r"] = true
+	}
+
+	// for _, x := range files{
+	// 	if Themap
+	// }
 }
+
